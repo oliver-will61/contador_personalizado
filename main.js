6068637c02a67e3543fad55450ddff058795388f
Willian de Oliveira Ribeiro <will.oliveira2408@gmail.com>
2024-01-18 19:17:06 -0300

    controles subtraido e somando

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const btnNegativo = document.getElementById('botao-negativo');
 const btnPostivo = document.getElementById('botao-positivo');
+const objetoQuantidade = document.getElementById('quantidade');
 
-const objetoQuantidade = document.getElementById('quantidade')
+const btnAdd = document.getElementById('add-obj');
+const btnSave = document.getElementById('botao-salvar');
+const btnLoad = document.getElementById('botao-load');
+
 
 btnPostivo.addEventListener('click', () => {
     objetoQuantidade.value = parseFloat(objetoQuantidade.value) +1
