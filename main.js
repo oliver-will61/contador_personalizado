a43cffc3578d8bac0f9495403ee60e263d60eb7f
Willian de Oliveira Ribeiro <will.oliveira2408@gmail.com>
2024-01-22 11:07:57 -0300

    desenvolvido o  btn de adicioanr novos contadores

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
-const btnNegativo = document.getElementById('botao-negativo');
-const btnPostivo = document.getElementById('botao-positivo');
-const objetoQuantidade = document.getElementById('quantidade');
+
+//const objetoQuantidade = document.querySelectorAll('#quantidade');
+const containerForm = document.getElementById("form");
+let btnPostivo = document.querySelectorAll('#botao-positivo')
+let btnNegativo = document.querySelectorAll('#botao-negativo')
 
 const btnAdd = document.getElementById('add-obj');
 const btnSave = document.getElementById('botao-salvar');
 const btnLoad = document.getElementById('botao-load');
 
+carregaBtns();
 
-btnPostivo.addEventListener('click', () => {
-    objetoQuantidade.value = parseFloat(objetoQuantidade.value) +1
+
+btnAdd.addEventListener('click', () => {
+
+    let nomeContador = prompt('Digite o nome do contador');
+
+    containerForm.innerHTML+= `            
+    <div class="objeto">
+        <h1 class="objeto-titulo" id= "objeto">${nomeContador}</h1>
+        <div class="controle">
+            <button type="button"class="objeto-botoes" id="botao-negativo">-</button>
+            <input readonly class="objeto-quantidade" id="quantidade" value="0">
+            <button type="button" class="objeto-botoes" id="botao-positivo">+</button>
+        </div>
+    </div> 
+`
+    carregaBtns()
+
 })
 
-btnNegativo.addEventListener('click', ()=> {
-    objetoQuantidade.value = parseFloat(objetoQuantidade.value) -1
-})
+function carregaBtns() {
+     btnNegativo = document.querySelectorAll('#botao-negativo');
+     btnPostivo = document.querySelectorAll('#botao-positivo');
+
+     btnPostivo.forEach((botao)=> {
+        botao.addEventListener('click', (event) => {
+           let objetoQuantidade = event.currentTarget.parentElement.querySelector('#quantidade')
+           objetoQuantidade.value = parseFloat(objetoQuantidade.value) +1
+        })
+    }) 
+
+    btnNegativo.forEach((botao)=>{
+        botao.addEventListener('click', (event)=> {
+            let objetoQuantidade = event.currentTarget.parentElement.querySelector('#quantidade');
+            objetoQuantidade.value = parseFloat(objetoQuantidade.value) -1
+        })
+    })
+}
+
